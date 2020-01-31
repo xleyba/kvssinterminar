104a88bfc11e95b587a0cc8da318e239e9fd1be7
Javier L <135663+xleyba@users.noreply.github.com>
2020-01-31 11:14:48 +0100

    added clap management

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,69 @@
+extern crate clap;
+
+use clap::{App, AppSettings, Arg, SubCommand};
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Kvs")
+        .version("0.1.0")
+        .author("Javier")
+        .about("Does awesome things")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("get")
+                .about("Get a value from a given key")
+                .arg(
+                    Arg::with_name("key")
+                        .takes_value(true)
+                        .required(true)
+                        .value_name("KEY")
+                        .help("the key to search for"),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("set a key/value pair")
+                .arg(
+                    Arg::with_name("key")
+                        .takes_value(true)
+                        .required(true)
+                        .value_name("KEY")
+                        .help("the key to set"),
+                )
+                .arg(
+                    Arg::with_name("value")
+                        .takes_value(true)
+                        .required(true)
+                        .value_name("VALUE")
+                        .help("the value to set"),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("rm")
+                .about("remove a key")
+                .arg(
+                    Arg::with_name("key")
+                        .takes_value(true)
+                        .required(true)
+                        .value_name("KEY")
+                        .help("the key to remove"),
+                ),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("set", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("get", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("rm", Some(_matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        _ => unreachable!(),
+    }
 }
